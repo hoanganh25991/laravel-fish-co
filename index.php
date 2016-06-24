cc8c7e70ccbfd7ab8c82b35ef9b153c5f11e407e
hoanganh25991 <lehoanganh25991@gmail.com>
2016-06-24 09:26:22 +0700

    allow access ^/upload/example.png, by change $uri logic in index.php, ONLY applied for rightfrom.us server

--- index.php
+++ index.php
@@ -11,9 +11,20 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
+
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
+
+/** ONLY apply for rightfrom.us server to find-out truth path of file */
+/*
+ * $uri = /fishco/upload/example.png
+ * logic check: file_exists(__DIR__.'/public'.$uri)
+ * __DIR__.'/public'.$uri = "/var/www/html/fishco/public[/fishco]upload/example.png
+ * WRONG at [/fishco]
+ * get out /fishco from $uri
+ */
+$uri = str_replace("/fishco", "", $uri);
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
