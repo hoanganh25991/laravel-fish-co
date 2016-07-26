33da4bd987db0436f91d157a04b7fa04b7578d46
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-07-26 09:39:39 +0700

    bak query.sql

--- query.sql
+++ query.sql
@@ -6,3 +6,11 @@ ALTER TABLE `like` ADD UNIQUE `unique_index`(`submission_id`, `device_id`);
 /* drop unique index */
 ALTER TABLE `like` DROP INDEX `unique_index`;
 
+/* view all_submission */
+select `submission`.`id` AS `id`,`candidate`.`name` AS `candidate name`,`candidate`.`email` 
+AS `candidate email`,`candidate`.`contact_number` AS `candidate contact number`,`image`.`path` 
+AS `image`,`country`.`name` AS `country name`,`submission`.`redeem_at` AS `redeem at` 
+from (((`submission` join `candidate` on((`submission`.`candidate_id` = `candidate`.`id`))) 
+	join `country` on((`submission`.`country_id` = `country`.`id`))) join `image` 
+on((`image`.`id` = `submission`.`image_id`)))
+
